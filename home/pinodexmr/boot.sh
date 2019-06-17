e98f58976036416eb3ec482e128d25ca7c49e506
shermand100 <shermand100@gmail.com>
2019-06-17 16:37:54 +0100

    add print onion address
    onion address printed earlier (formerly in init.sh

--- home/pinodexmr/boot.sh
+++ home/pinodexmr/boot.sh
@@ -5,13 +5,18 @@
 if [ $BOOT_STATUS -lt 1 ]
 then
 	sudo raspi-config --expand-rootfs
+	sleep "120"
 	echo "#!/bin/sh
 BOOT_STATUS=2" > /home/pinodexmr/bootstatus.sh
-	sleep "120"
+	sleep "2"
+	#Output onion address
+sudo cat /var/lib/tor/hidden_service/hostname > /var/www/html/onion-address.txt
+	sleep "2"
 	sudo reboot
 	
 else
 . /home/pinodexmr/df-h.sh
 . /home/pinodexmr/init.sh	
 fi
+
 
