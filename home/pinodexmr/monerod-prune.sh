ceb240f0a75f88f1ac6c002a7885f2519db82607
shermand100 <shermand100@gmail.com>
2019-06-18 15:50:30 +0100

    Prune command cleaned up, previous invalid
    original had bad path for start
    
    Prune button removed as one-off command.
    
    User is better to use web terminal to view readout of progress of prune. avoid suspicion of crash and cause hard restart by user.

--- home/pinodexmr/monerod-prune.sh
+++ home/pinodexmr/monerod-prune.sh
@@ -8,13 +8,14 @@ sleep "1"
 if [ $PRUNE_STATUS -lt 1 ]
 then
 #Start prune binary
-	. /home/pinodexmr/monero/monero-blockchain-prune
+cd /home/pinodexmr/monero/
+./monero-blockchain-prune
 	
 #Update prune status to show binary run
 	echo "#!/bin/bash
 PRUNE_STATUS=1" > /home/pinodexmr/prunestatus.sh
 #Show UI flag for user info
-	echo "The pruning function has already been enabled and cannot be run again with this button. Please consult the manual for more info" > /var/www/html/prune-text.txt
+	echo "The pruning function has already been enabled and cannot be run again with this command. Please consult the manual for more info" > /var/www/html/prune-text.txt
 #Append pruning to all start flags
 echo " --prune-blockchain" >> /home/pinodexmr/monerod-start.sh
 echo " --prune-blockchain" >> /home/pinodexmr/monerod-start-tor.sh
