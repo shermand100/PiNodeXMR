2f5db2fc19f7844dae2cf694f7fd650e2101f259
shermand100 <shermand100@gmail.com>
2020-01-02 22:55:43 +0000

    New path for monerod armv7

--- home/pinodexmr/node_version.sh
+++ home/pinodexmr/node_version.sh
@@ -1,7 +1,22 @@
 #!/bin/sh
+	#Load boot status - what condition was node last run
+	. /home/pinodexmr/bootstatus.sh
+	#Import Un-restricted Port Number (for internal status updates)
+	. /home/pinodexmr/monero-stats-port.sh
+	#Import Restricted Port Number (external use)
+	. /home/pinodexmr/monero-port.sh
 	#Import RPC username
 	. /home/pinodexmr/RPCu.sh
 	#Import RPC password
 	. /home/pinodexmr/RPCp.sh
-#Node Version Print
-./monero/monerod --rpc-bind-ip=$(hostname -I) --rpc-login=$RPCu:$RPCp  --rpc-ssl disabled version | sed '1d' > /var/www/html/node_version.txt
+
+
+	if [ $BOOT_STATUS -eq 6 ]
+then
+		#Adapted command for restricted public rpc calls (payments)
+	./monero-active/monerod --rpc-bind-ip=$(hostname -I) --rpc-bind-port=$MONERO_STATS_PORT --rpc-ssl disabled version | sed '1d' > /var/www/html/node_version.txt
+else
+		#Node Version Print
+		./monero-active/monerod --rpc-bind-ip=$(hostname -I) --rpc-bind-port=$MONERO_PORT --rpc-login=$RPCu:$RPCp --rpc-ssl disabled version | sed '1d' > /var/www/html/node_version.txt
+			
+fi 
--- home/pinodexmr/print_pl.sh
+++ home/pinodexmr/print_pl.sh
@@ -1,7 +1,22 @@
 #!/bin/sh
+#Import Start Flag Values:
+	#Load boot status - what condition was node last run
+	. /home/pinodexmr/bootstatus.sh
+	#Import Un-restricted Port Number (for internal status updates)
+	. /home/pinodexmr/monero-stats-port.sh
+	#Import Restricted Port Number (external use)
+	. /home/pinodexmr/monero-port.sh
 	#Import RPC username
 	. /home/pinodexmr/RPCu.sh
 	#Import RPC password
 	. /home/pinodexmr/RPCp.sh
-#Print all white/grey nodes
-./monero/monerod --rpc-bind-ip=$(hostname -I) --rpc-login $RPCu:$RPCp --rpc-ssl disabled print_pl | sed '1d' > /var/www/html/print_pl.txt
+	
+		if [ $BOOT_STATUS -eq 6 ]
+then
+		#Adapted command for restricted public rpc calls (payments)
+	./monero-active/monerod --rpc-bind-ip=$(hostname -I) --rpc-bind-port=$MONERO_STATS_PORT --rpc-ssl disabled print_pl | sed '1d' > /var/www/html/print_pl.txt
+else
+	#Print all white/grey nodes
+	./monero-active/monerod --rpc-bind-ip=$(hostname -I) --rpc-bind-port=$MONERO_PORT --rpc-login=$RPCu:$RPCp --rpc-ssl disabled print_pl | sed '1d' > /var/www/html/print_pl.txt	
+fi
+
