9da972c726ec9c44f6a38d08d0fe16ed5d0ba2d9
shermand100 <shermand100@gmail.com>
2020-01-09 01:19:45 +0000

    update updater script

--- update-pinodexmr.sh
+++ update-pinodexmr.sh
@@ -1,16 +1,11 @@
 #!/bin/bash
 
-whiptail --title "PiNode-XMR Update" --msgbox "\n\n" 12 78
-
 if (whiptail --title "PiNode-XMR Updater" --yesno "This will update PiNode-XMR to the newest version\n\nContinue?" 12 78); then
-
+		echo "Commands to be inserted here for updating PiNode-XMR with new features"
+		sleep 5
 else
     . /home/pinodexmr/setup.sh
 fi
-		echo "Commands to be inserted here for updating PiNode-XMR with new features"
-		sleep 5
-		else
-	
 
 #Return to menu
 ./setup.sh
