738cd453c23e0c40fe8bea4bf928b7488b2e2a87
shermand100 <shermand100@gmail.com>
2020-02-29 16:34:31 +0000

    Give Raspbian install it's own brach, links changed

--- raspbian.sh
+++ raspbian.sh
@@ -24,7 +24,7 @@ sleep 3
 ##Replace file /etc/sudoers to set global sudo permissions/rules
 echo -e "\e[32mDownload and replace /etc/sudoers file\e[0m"
 sleep 3
-wget https://raw.githubusercontent.com/shermand100/pinode-xmr/development/etc/sudoers
+wget https://raw.githubusercontent.com/shermand100/pinode-xmr/Raspbian-install/etc/sudoers
 sudo chmod 0440 /home/pi/sudoers
 sudo chown root /home/pi/sudoers
 sudo mv /home/pi/sudoers /etc/sudoers
@@ -32,7 +32,7 @@ echo -e "\e[32mGlobal permissions changed\e[0m"
 sleep 3
 
 ##Download user 'pinodexmr' script and put in home directory to continue
-wget https://raw.githubusercontent.com/shermand100/pinode-xmr/development/raspbian-pinodexmr.sh
+wget https://raw.githubusercontent.com/shermand100/pinode-xmr/Raspbian-install/raspbian-pinodexmr.sh
 sudo mv /home/pi/raspbian-pinodexmr.sh /home/pinodexmr/
 sudo chown pinodexmr /home/pinodexmr/raspbian-pinodexmr.sh
 sudo chmod 755 /home/pinodexmr/raspbian-pinodexmr.sh
