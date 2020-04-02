3e5e5998bafdf5b2d72ce25e9bcc5a1aba2dafb1
shermand100 <shermand100@gmail.com>
2020-04-02 17:03:26 +0100

    Create foundations of part installer

--- armbian-install-continue.sh
+++ armbian-install-continue.sh
@@ -7,27 +7,7 @@
 # PiNode-XMR scripts and custom files at my repo https://github.com/shermand100/pinode-xmr
 # PiVPN - OpenVPN server setup https://github.com/pivpn/pivpn
 
-###Begin
-
-
-
-##Replace file /etc/sudoers to set global sudo permissions/rules
-echo -e "\e[32mDownload and replace /etc/sudoers file\e[0m"
-sleep 3
-wget https://raw.githubusercontent.com/shermand100/pinode-xmr/Armbian-install/etc/sudoers
-sudo chmod 0440 /home/pinodexmr/sudoers
-sudo chown root /home/pinodexmr/sudoers
-sudo mv /home/pinodexmr/sudoers /etc/sudoers
-echo -e "\e[32mGlobal permissions changed\e[0m"
-sleep 3
-
-##Change system hostname to PiNodeXMR
-echo -e "\e[32mChanging system hostname to 'PiNodeXMR'\e[0m"
-sleep 3
-echo 'PiNodeXMR' | sudo tee /etc/hostname
-#sudo sed -i '6d' /etc/hosts
-echo '127.0.0.1       PiNodeXMR' | sudo tee -a /etc/hosts
-sudo hostname PiNodeXMR
+###Begin2
 
 ##Update and Upgrade system
 echo -e "\e[32mReceiving and applying Armbian updates to latest versions\e[0m"
