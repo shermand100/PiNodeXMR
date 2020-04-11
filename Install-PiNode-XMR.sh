0ebb1ac2394caffa22b83ee7c1eb52c8009837cc
shermand100 <shermand100@gmail.com>
2020-04-11 19:37:56 +0100

    Update links to monero-ecosystem

--- Install-PiNode-XMR.sh
+++ Install-PiNode-XMR.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-##To run me on cmd line wget -O - https://raw.githubusercontent.com/shermand100/pinode-xmr/master/Install-PiNode-XMR.sh | bash
+##To run me on cmd line wget -O - https://raw.githubusercontent.com/monero-ecosystem/PiNode-XMR/master/Install-PiNode-XMR.sh | bash
 echo -e "\e[32mPreparing Menu...\e[0m"
 sleep 2
 #Check user has whiptail - required to display menu
@@ -18,7 +18,7 @@ case $CHOICE in
 		#Commands for Raspbian
 		echo -e "\e[32mDownloading data for install\e[0m"
 		sleep 3
-		wget https://raw.githubusercontent.com/shermand100/pinode-xmr/Raspbian-install/raspbian.sh
+		wget https://raw.githubusercontent.com/monero-ecosystem/PiNode-XMR/Raspbian-install/raspbian.sh
 		echo -e "\e[32mPiNode-XMR Raspbian configuration file received\e[0m"
 		echo -e "\e[32mStarting Installation\e[0m"
 		sudo chmod 755 /home/pi/raspbian.sh
@@ -36,7 +36,7 @@ case $CHOICE in
 		#Commands for Armbian
 		echo -e "\e[32mDownloading data for install\e[0m"
 		sleep 3
-		wget https://raw.githubusercontent.com/shermand100/pinode-xmr/Armbian-install/armbian-installer.sh
+		wget https://raw.githubusercontent.com/monero-ecosystem/PiNode-XMR/Armbian-install/armbian-installer.sh
 		echo -e "\e[32mPiNode-XMR Armbian configuration file received\e[0m"
 		echo -e "\e[32mStarting Installation\e[0m"
 		sudo chmod 755 ~/armbian-installer.sh
