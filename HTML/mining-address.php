275cf67e7f2eb347de6b034b73189a1147f6ab6d
shermand100 <shermand100@gmail.com>
2020-04-13 17:21:58 +0100

    Better advanced settings page, interactive fields

--- HTML/mining-address.php
+++ HTML/mining-address.php
@@ -4,5 +4,10 @@ $fp = fopen('/home/pinodexmr/mining-address.sh', 'w');
 fwrite($fp, "#!/bin/bash\nMINING_ADDRESS=$VALUE
 ");
 fclose($fp);
+
+$fpa = fopen('/var/www/html/mining_address.txt', 'w');
+fwrite($fpa, "Currently set to $VALUE");
+fclose($fpa);
+
 echo "Mining address set to $VALUE ";
  ?>
---
+++ HTML/mining-intensity.php
@@ -0,0 +1,13 @@
+<?php
+$VALUE = $_POST["value"];
+$fp = fopen('/home/pinodexmr/mining-intensity.sh', 'w');
+fwrite($fp, "#!/bin/bash\nMINING_INTENSITY=$VALUE
+");
+fclose($fp);
+
+$fpa = fopen('/var/www/html/mining_intensity.txt', 'w');
+fwrite($fpa, "Currently set to $VALUE percent");
+fclose($fpa);
+
+echo "Mining intensity set to $VALUE percent ";
+ ?>
