9c151e6d5a1e6b2d165cb4ab9064ebcac58afdbb
shermand100 <shermand100@gmail.com>
2020-05-24 15:49:00 +0100

    fix 2 to updater, broken links

--- update-pinodexmr.sh
+++ update-pinodexmr.sh
@@ -5,14 +5,12 @@ if (whiptail --title "PiNode-XMR Updater" --yesno "This will force update PiNode
 		#Download update files
 
 					https://raw.githubusercontent.com/monero-ecosystem/PiNode-XMR/Raspbian-install/new-ver-pi.sh -O /home/pinodexmr/new-ver-pi.sh
-					chmod 755 /home/pinodexmr/new-ver-pi.sh
-					. /home/pinodexmr/new-ver-pi.sh
+
 					echo "Latest Version: $NEW_VERSION_PI "
 					echo -e "\e[32mDownloading PiNode-XMR files\e[0m"
 					sleep 2
 					
 					git clone -b Raspbian-install --single-branch https://github.com/monero-ecosystem/PiNode-XMR.git
-					wget https://raw.githubusercontent.com/shermand100/pinode-xmr/Raspbian-install/new-ver-pi.sh -O /home/pinodexmr/new-ver-pi.sh
 
 					#Backup User values
 					echo -e "\e[32mCreating backups of your configuration\e[0m"
@@ -44,7 +42,7 @@ if (whiptail --title "PiNode-XMR Updater" --yesno "This will force update PiNode
 						##Update PiNode-XMR systemd services
 					echo -e "\e[32mUpdating PiNode-XMR systemd services\e[0m"
 					sleep 2
-					sudo mv /home/pinodexmr/pinode-xmr/etc/systemd/system/*.service /etc/systemd/system/
+					sudo mv /home/pinodexmr/PiNode-XMR/etc/systemd/system/*.service /etc/systemd/system/
 					sudo chmod 644 /etc/systemd/system/*.service
 					sudo chown root /etc/systemd/system/*.service
 					echo -e "\e[32mSuccess\e[0m"
@@ -53,16 +51,16 @@ if (whiptail --title "PiNode-XMR Updater" --yesno "This will force update PiNode
 					echo -e "\e[32mUpdating PiNode-XMR scripts in home directory\e[0m"
 					sleep 2
 					sudo rm -R /home/pinodexmr/flock #if folder not removed produces error, cannot be overwritten if not empty
-					mv /home/pinodexmr/pinode-xmr/home/pinodexmr/* /home/pinodexmr/
-					mv /home/pinodexmr/pinode-xmr/home/pinodexmr/.profile /home/pinodexmr/
+					mv /home/pinodexmr/PiNode-XMR/home/pinodexmr/* /home/pinodexmr/
+					mv /home/pinodexmr/PiNode-XMR/home/pinodexmr/.profile /home/pinodexmr/
 					chmod 777 /home/pinodexmr/*
 					echo -e "\e[32mSuccess\e[0m"
 					sleep 2
 						##Update web interface
 					echo -e "\e[32mUpdating your Web Interface\e[0m"
 					sleep 2
-					sudo mv /home/pinodexmr/pinode-xmr/HTML/*.* /var/www/html/
-					sudo cp -R /home/pinodexmr/pinode-xmr/HTML/docs/ /var/www/html/
+					sudo mv /home/pinodexmr/PiNode-XMR/HTML/*.* /var/www/html/
+					sudo cp -R /home/pinodexmr/PiNode-XMR/HTML/docs/ /var/www/html/
 					sudo chown www-data -R /var/www/html/
 					sudo chmod 777 -R /var/www/html/
 					echo -e "\e[32mSuccess\e[0m"
@@ -96,8 +94,8 @@ if (whiptail --title "PiNode-XMR Updater" --yesno "This will force update PiNode
 					##Update crontab
 					echo -e "\e[32mSetup crontab\e[0m"
 					sleep 3
-					sudo crontab /home/pinodexmr/pinode-xmr/var/spool/cron/crontabs/root
-					crontab /home/pinodexmr/pinode-xmr/var/spool/cron/crontabs/pinodexmr
+					sudo crontab /home/pinodexmr/PiNode-XMR/var/spool/cron/crontabs/root
+					crontab /home/pinodexmr/PiNode-XMR/var/spool/cron/crontabs/pinodexmr
 					echo -e "\e[32mSuccess\e[0m"
 					sleep 3
 
@@ -118,7 +116,7 @@ CURRENT_VERSION=0" > /home/pinodexmr/current-ver.sh
 					#Clean up files
 					echo -e "\e[32mCleanup leftover directories\e[0m"
 					sleep 2
-					sudo rm -r /home/pinodexmr/pinode-xmr/
+					sudo rm -r /home/pinodexmr/PiNode-XMR/
 					rm /home/pinodexmr/new-ver-pi.sh
 					echo -e "\e[32mSuccess\e[0m"
 					sleep 2
