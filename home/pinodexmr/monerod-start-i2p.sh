784f3c892d136bb6d60c2477b5f0ead9f7a3f201
shermand100 <shermand100@gmail.com>
2020-08-08 15:37:47 +0100

    Improved funtionality with --anonymous-inbound

--- home/pinodexmr/monerod-start-i2p.sh
+++ home/pinodexmr/monerod-start-i2p.sh
@@ -2,7 +2,7 @@
 #Establish IP
 DEVICE_IP="$(hostname -I | awk '{print $1}')"
 #Import Start Flag Values:
-	#Import Port Number
+	#Import RPC Port Number
 	. /home/pinodexmr/monero-port.sh
 	#Import IN-PEERS (connections) Limit
 	. /home/pinodexmr/in-peers.sh
@@ -20,10 +20,10 @@ DEVICE_IP="$(hostname -I | awk '{print $1}')"
 	. /home/pinodexmr/i2p-address.sh
 	#Import your I2P server/router port
 	. /home/pinodexmr/i2p-port.sh
-	#Import tx-proxy port
+	#Import i2p-tx-proxy port
 	. /home/pinodexmr/i2p-tx-proxy-port.sh
-	#Import ADD_PEER (seed) port
-	. /home/pinodexmr/add-peer.sh
+	#Import ADD_I2P_PEER (seed) port
+	. /home/pinodexmr/add-i2p-peer.sh
 
 #Update power cycle reboot state
 	echo "#!/bin/sh
@@ -31,4 +31,4 @@ BOOT_STATUS=8" > /home/pinodexmr/bootstatus.sh
 #Start Monerod
 cd /home/pinodexmr/monero/build/release/bin/
 
-DNS_PUBLIC=tcp ./monerod --p2p-bind-ip 127.0.0.1 --rpc-bind-ip=127.0.0.1 --rpc-bind-port=$I2P_PORT --rpc-login=$RPCu:$RPCp --confirm-external-bind --anonymous-inbound $I2P_ADDRESS:$I2P_PORT,$DEVICE_IP:18080 --tx-proxy i2p,127.0.0.1:$I2P_TX_PROXY_PORT --rpc-ssl disabled --log-level=1 --add-peer $ADD_PEER --in-peers=$IN_PEERS --out-peers=$OUT_PEERS --limit-rate-up=$LIMIT_RATE_UP --limit-rate-down=$LIMIT_RATE_DOWN --log-file=/var/www/html/monerod.log --max-log-file-size=10485000 --max-log-files=1 --pidfile /home/pinodexmr/monero/build/release/bin/monerod.pid --detach
+DNS_PUBLIC=tcp ./monerod --p2p-bind-ip 127.0.0.1 --rpc-bind-ip=$DEVICE_IP --rpc-bind-port=$MONERO_PORT --rpc-login=$RPCu:$RPCp --confirm-external-bind --anonymous-inbound $I2P_ADDRESS,127.0.0.1:$I2P_PORT --tx-proxy i2p,127.0.0.1:$I2P_TX_PROXY_PORT --rpc-ssl disabled --log-level=1 --add-peer $ADD_I2P_PEER --in-peers=$IN_PEERS --out-peers=$OUT_PEERS --limit-rate-up=$LIMIT_RATE_UP --limit-rate-down=$LIMIT_RATE_DOWN --log-file=/var/www/html/monerod.log --max-log-file-size=10485000 --max-log-files=1 --pidfile /home/pinodexmr/monero/build/release/bin/monerod.pid --detach
