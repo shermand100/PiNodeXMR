672ff84a52af73a2684126c962941fb3fc2f7171
shermand100 <shermand100@gmail.com>
2020-08-08 15:38:20 +0100

    Cleaner readout with cut of leading blank space

--- home/pinodexmr/node_version.sh
+++ home/pinodexmr/node_version.sh
@@ -15,30 +15,31 @@ DEVICE_IP="$(hostname -I | awk '{print $1}')"
 	if [ $BOOT_STATUS -eq 2 ]
 then	
 		#Node Version Print
-		VERSION="$(./monero/build/release/bin/monerod --rpc-bind-ip=${DEVICE_IP} --rpc-bind-port=${MONERO_PORT} --rpc-login=${RPCu}:${RPCp} --rpc-ssl disabled version | sed -n '2p' | sed -n 's/Monero/&/p')" && echo "${VERSION}" > /var/www/html/node_version.txt
+		VERSION="$(./monero/build/release/bin/monerod --rpc-bind-ip=${DEVICE_IP} --rpc-bind-port=${MONERO_PORT} --rpc-login=${RPCu}:${RPCp} --rpc-ssl disabled version | sed -n '2p' | cut -c 5-)" && echo "${VERSION}" > /var/www/html/node_version.txt
 fi	
 	
 	
 	if [ $BOOT_STATUS -eq 3 ] || [ $BOOT_STATUS -eq 5 ]
 then	
 		#Node Status
-		VERSION="$(./monero/build/release/bin/monerod --rpc-bind-ip=${DEVICE_IP} --rpc-bind-port=${MONERO_PORT} --rpc-login=${RPCu}:${RPCp} --rpc-ssl disabled version | sed -n '2p' | sed -n 's/Monero/&/p')" && echo "${VERSION}" > /var/www/html/node_version.txt
+		VERSION="$(./monero/build/release/bin/monerod --rpc-bind-ip=${DEVICE_IP} --rpc-bind-port=${MONERO_PORT} --rpc-login=${RPCu}:${RPCp} --rpc-ssl disabled version | sed -n '2p' | cut -c 5-)" && echo "${VERSION}" > /var/www/html/node_version.txt
 fi	
 
 	if [ $BOOT_STATUS -eq 4 ] || [ $BOOT_STATUS -eq 8 ]
 then	
 		#Node Status
-		VERSION="$(./monero/build/release/bin/monerod --rpc-bind-ip=127.0.0.1 --rpc-bind-port=18081 --rpc-login=${RPCu}:${RPCp} --rpc-ssl disabled version | sed -n '2p' | sed -n 's/Monero/&/p')" && echo "${VERSION}" > /var/www/html/node_version.txt
+		VERSION="$(./monero/build/release/bin/monerod --rpc-bind-ip=${DEVICE_IP} --rpc-bind-port=18081 --rpc-login=${RPCu}:${RPCp} --rpc-ssl disabled version | sed -n '2p' | cut -c 5-)" && echo "${VERSION}" > /var/www/html/node_version.txt;
+
 fi
 	
 	if [ $BOOT_STATUS -eq 6 ]
 then
 		#Adapted command for restricted public rpc calls (payments)
-		VERSION="$(./monero/build/release/bin/monerod --rpc-bind-ip=$DEVICE_IP --rpc-bind-port=$MONERO_STATS_PORT --rpc-ssl disabled version | sed -n '2p' | sed -n 's/Monero/&/p')" && echo "${VERSION}" > /var/www/html/node_version.txt
+		VERSION="$(./monero/build/release/bin/monerod --rpc-bind-ip=$DEVICE_IP --rpc-bind-port=$MONERO_STATS_PORT --rpc-ssl disabled version | sed -n '2p' | cut -c 5-)" && echo "${VERSION}" > /var/www/html/node_version.txt
 fi
 
 	if [ $BOOT_STATUS -eq 7 ]
 then
 		#Adapted command for public free (restricted) rpc calls. No auth needed for local.
-		VERSION="$(./monero/build/release/bin/monerod --rpc-bind-ip=$DEVICE_IP --rpc-bind-port=$MONERO_PORT --rpc-ssl disabled version | sed -n '2p' | sed -n 's/Monero/&/p')" && echo "${VERSION}" > /var/www/html/node_version.txt
+		VERSION="$(./monero/build/release/bin/monerod --rpc-bind-ip=$DEVICE_IP --rpc-bind-port=$MONERO_PORT --rpc-ssl disabled version | sed -n '2p' | cut -c 5-)" && echo "${VERSION}" > /var/www/html/node_version.txt
 fi
