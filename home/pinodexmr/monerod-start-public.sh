a2ae15a119752ef137f3fbe7796eabbdf7335119
shermand100 <shermand100@gmail.com>
2020-08-10 08:34:57 +0100

    Update to specify rpc_pay port on start

--- home/pinodexmr/monerod-start-public.sh
+++ home/pinodexmr/monerod-start-public.sh
@@ -2,8 +2,8 @@
 #Establish IP
 DEVICE_IP="$(hostname -I | awk '{print $1}')"
 #Import Start Flag Values:
-	#Import stats Port Number
-	. /home/pinodexmr/monero-stats-port.sh
+	#Import RPC Pay Port Number
+	. /home/pinodexmr/monero-rpcpay-port.sh
 	#Import restricted Port Number
 	. /home/pinodexmr/monero-port.sh
 	#Import IN-PEERS (connections) Limit
@@ -30,4 +30,4 @@ DEVICE_IP="$(hostname -I | awk '{print $1}')"
 BOOT_STATUS=6" > /home/pinodexmr/bootstatus.sh
 #Start Monerod
 cd /home/pinodexmr/monero/build/release/bin/
-./monerod --rpc-bind-ip=$DEVICE_IP --rpc-bind-port=$MONERO_STATS_PORT --rpc-restricted-bind-port=$MONERO_PORT --confirm-external-bind --rpc-payment-allow-free-loopback --rpc-ssl disabled --in-peers=$IN_PEERS --out-peers=$OUT_PEERS --limit-rate-up=$LIMIT_RATE_UP --limit-rate-down=$LIMIT_RATE_DOWN --log-file=/var/www/html/monerod.log --max-log-file-size=10485000  --log-level=1 --max-log-files=1 --pidfile /home/pinodexmr/monero/build/release/bin/monerod.pid --rpc-payment-address=$PAYMENT_ADDRESS --rpc-payment-credits=$CREDITS --rpc-payment-difficulty=$DIFFICULTY --detach
+./monerod --rpc-bind-ip=$DEVICE_IP --rpc-bind-port=$MONERO_PORT --rpc-restricted-bind-port=$MONERO_RPCPAY_PORT --confirm-external-bind --rpc-payment-allow-free-loopback --rpc-ssl disabled --in-peers=$IN_PEERS --out-peers=$OUT_PEERS --limit-rate-up=$LIMIT_RATE_UP --limit-rate-down=$LIMIT_RATE_DOWN --log-file=/var/www/html/monerod.log --max-log-file-size=10485000  --log-level=1 --max-log-files=1 --pidfile /home/pinodexmr/monero/build/release/bin/monerod.pid --rpc-payment-address=$PAYMENT_ADDRESS --rpc-payment-credits=$CREDITS --rpc-payment-difficulty=$DIFFICULTY --detach
