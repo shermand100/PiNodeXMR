7432569adba2e866486cb1534008e1e7d6cab673
shermand100 <shermand100@gmail.com>
2020-08-10 08:40:30 +0100

    Add force update option
    force update added to allow more user control and ability of "factory reset"

--- home/pinodexmr/setup-update-monero.sh
+++ home/pinodexmr/setup-update-monero.sh
@@ -13,6 +13,8 @@ wget -q https://raw.githubusercontent.com/monero-ecosystem/PiNode-XMR/master/rel
 chmod 755 /home/pinodexmr/current-ver.sh
 chmod 755 /home/pinodexmr/xmr-new-ver.sh
 chmod 755 /home/pinodexmr/release.sh
+#Load boot status - what condition was node last run
+. /home/pinodexmr/bootstatus.sh
 #Load Variables
 . /home/pinodexmr/current-ver.sh
 . /home/pinodexmr/xmr-new-ver.sh
@@ -25,8 +27,11 @@ sleep "3"
 		echo -e "\e[32mNew Monero version available...Proceeding with download\e[0m"
 		sleep "2"
 		sudo systemctl stop monerod-start.service
+		sudo systemctl stop monerod-prune.service
+		sudo systemctl stop monerod-start-free.service
 		sudo systemctl stop monerod-start-mining.service
 		sudo systemctl stop monerod-start-tor.service
+		sudo systemctl stop monerod-start-i2p.service
 		sudo systemctl stop monerod-start-public.service
 		sudo systemctl stop explorer-start.service
 		echo "Monerod stop command sent, allowing 30 seconds for safe shutdown"
@@ -42,11 +47,14 @@ sleep "3"
 		USE_SINGLE_BUILDDIR=1 make
 		cd
 		
-		echo "Software Update Complete - Resuming Node"
 		sleep 2
+		if [ $BOOT_STATUS -eq 2 ]
+then
+		echo "Update complete, Node ready for start. See web-ui at $(hostname -I) to select mode."
+else
 		. /home/pinodexmr/init.sh
-		echo "Monero Node Started in background"
-
+		echo "Resuming Node"	
+fi
 		#Update system version number
 		echo "#!/bin/bash
 		CURRENT_VERSION=$NEW_VERSION" > /home/pinodexmr/current-ver.sh
@@ -56,8 +64,54 @@ sleep "3"
 		whiptail --title "PiNode-XMR Monero Update Complete" --msgbox "Your PiNode-XMR has completed updating to the latest version of Monero" 16 60
 		sleep 3
 else
+				
+		if (whiptail --title "PiNode-XMR Monero Update" --yesno "This device thinks it's running the latest version of Monero (that has been tested with PiNode-XMR).\n\nIf you think this is incorrect or require a clean Monero install you may force an update below." --yes-button "Force Monero Update" --no-button "Return to Main Menu"  14 78); then
+
+		echo -e "\e[32mSystem will now install the latest available of Monero.\e[0m"
+		sleep "2"
+		sudo systemctl stop monerod-start.service
+		sudo systemctl stop monerod-prune.service
+		sudo systemctl stop monerod-start-free.service
+		sudo systemctl stop monerod-start-mining.service
+		sudo systemctl stop monerod-start-tor.service
+		sudo systemctl stop monerod-start-i2p.service
+		sudo systemctl stop monerod-start-public.service
+		sudo systemctl stop explorer-start.service
+		echo "Monerod stop command sent, allowing 30 seconds for safe shutdown"
+		sleep "30"
+		echo "Deleting Old Version"
+		rm -rf /home/pinodexmr/monero/
+		sleep "2"
+		echo "Ensuring swap-file enabled for Monero build"
+		sudo dphys-swapfile swapon
+		echo "Downloading latest Monero version"
+		git clone --recursive -b $RELEASE https://github.com/monero-project/monero.git
+		cd monero
+		USE_SINGLE_BUILDDIR=1 make
+		cd
+		
+		if [ $BOOT_STATUS -eq 2 ]
+then
+		echo "Update complete, Node ready for start. See web-ui at $(hostname -I) to select mode."
+else
+		. /home/pinodexmr/init.sh
+		echo "Resuming Node"	
+fi
+
+		#Update system version number
+		echo "#!/bin/bash
+		CURRENT_VERSION=$NEW_VERSION" > /home/pinodexmr/current-ver.sh
+		#Remove downloaded version check files
+		rm /home/pinodexmr/release.sh
 		rm /home/pinodexmr/xmr-new-ver.sh
-		whiptail --title "PiNode-XMR Up-to-date" --msgbox "Your PiNode-XMR is running the latest version of Monero" 16 60
+		whiptail --title "PiNode-XMR Monero Update Complete" --msgbox "Your PiNode-XMR has completed updating to the latest version of Monero" 16 60
+		sleep 3
+									else
+										rm /home/pinodexmr/release.sh
+										rm /home/pinodexmr/xmr-new-ver.sh
+									whiptail --title "PiNode-XMR Monero Update" --msgbox "Returning to Main Menu. No changes have been made." 12 78;
+									. /home/pinodexmr/setup.sh
+									fi
 
 fi
 ./setup.sh
