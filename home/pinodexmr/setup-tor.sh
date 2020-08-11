13d9fc9ebd43ab3be5e93d2c35a1fe605bbde841
shermand100 <shermand100@gmail.com>
2020-08-11 12:05:51 +0100

    Fix development branch source flag

--- home/pinodexmr/setup-tor.sh
+++ home/pinodexmr/setup-tor.sh
@@ -8,7 +8,7 @@ sleep 3
 sudo apt install tor torsocks nyx -y
 echo -e "\e[32mDownloading PiNode-XMR config file\e[0m"
 sleep 3
-wget https://raw.githubusercontent.com/monero-ecosystem/PiNode-XMR/Raspbian-install/etc/tor/torrc
+wget https://raw.githubusercontent.com/monero-ecosystem/PiNode-XMR/Development-Raspbian/etc/tor/torrc
 echo -e "\e[32mApplying Settings...\e[0m"
 sleep 3
 sudo mv /home/pinodexmr/torrc /etc/tor/torrc
