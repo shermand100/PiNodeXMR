9e6f7cedeb3075b361ca2c17464f45107e9dc6b3
ChiefGyk <19499446+ChiefGyk@users.noreply.github.com>
2020-10-26 19:42:17 -0400

    Uses the hardware agnostic /sys/devices/virtual/thermal/thermal_zone0/temp readout then divides by 1000 and should work regardless of hardware used. Even tested it on my Intel powered laptop.

--- home/pinodexmr/temp.sh
+++ home/pinodexmr/temp.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
 #CPU temp Status
-{ sudo /opt/vc/bin/vcgencmd measure_temp & echo "Updates every 60 seconds" & date; } > /var/www/html/temp.txt
+{
+temp=$(cat /sys/devices/virtual/thermal/thermal_zone0/temp)
+echo $(( temp / 1000 )) C
+echo "Updates every 60 seconds" &
+date;
+} > test.txt
