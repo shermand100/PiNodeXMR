88b60c2a46bc287d63bc4d4633032d632b8a2fbc
ChiefGyk <19499446+ChiefGyk@users.noreply.github.com>
2020-10-26 21:11:09 -0400

    updated paths correctly from testing, as well as the ordering of the display. May look at gathering hdd temp optionally

--- home/pinodexmr/temp.sh
+++ home/pinodexmr/temp.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 #CPU temp Status
 {
-temp=$(cat /sys/devices/virtual/thermal/thermal_zone0/temp)
-echo $(( temp / 1000 )) C
 echo "Updates every 60 seconds" &
+temp=$(cat /sys/devices/virtual/thermal/thermal_zone0/temp)
+echo $(( temp / 1000 )) C &
 date;
-} > test.txt
+} > /var/www/html/temp.txt
