c091a163f2e3d5894c0937038f983cfde218c37b
shermand100 <shermand100@gmail.com>
2020-11-30 17:06:30 +0000

    remove eronious ","

--- home/pinodexmr/NodeGeoIP/europegeoipmap.sh
+++ home/pinodexmr/NodeGeoIP/europegeoipmap.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-python3 /home/pinodexmr/NodeGeoIP/europegeoipmap.py -i /home/pinodexmr/NodeGeoIP/IPoutput.txt --extents=-12/45/30/65 --service m --db /home/pinodexmr/NodeGeoIP/GeoIPData/GeoLite2-City.mmdb --output=/var/www/html/europenodes.png,
+python3 /home/pinodexmr/NodeGeoIP/europegeoipmap.py -i /home/pinodexmr/NodeGeoIP/IPoutput.txt --extents=-12/45/30/65 --service m --db /home/pinodexmr/NodeGeoIP/GeoIPData/GeoLite2-City.mmdb --output=/var/www/html/europenodes.png
 
 	#Update timestamp of when list last created
 echo "Map last updated: " > /var/www/html/europe-map-timestamp.txt
