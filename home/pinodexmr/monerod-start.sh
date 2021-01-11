75f4765844cc617ce5472667bfb7e185ce9adbef
shermand100 <shermand100@gmail.com>
2021-01-11 22:53:56 +0000

    More robust start flags.
    Added --enable-dns-blocklist flag to all start commands.
    
    Also added --public-node flag to the free and RPC pay public nodes

--- home/pinodexmr/monerod-start.sh
+++ home/pinodexmr/monerod-start.sh
@@ -22,4 +22,4 @@ DEVICE_IP="$(hostname -I | awk '{print $1}')"
 BOOT_STATUS=3" > /home/pinodexmr/bootstatus.sh
 #Start Monerod
 cd /home/pinodexmr/monero/build/release/bin/
-./monerod --rpc-bind-ip=$DEVICE_IP --rpc-bind-port=$MONERO_PORT --confirm-external-bind --rpc-login=$RPCu:$RPCp --rpc-ssl disabled --in-peers=$IN_PEERS --out-peers=$OUT_PEERS --limit-rate-up=$LIMIT_RATE_UP --limit-rate-down=$LIMIT_RATE_DOWN --log-file=/var/www/html/monerod.log --max-log-file-size=10485000  --log-level=1 --max-log-files=1 --pidfile /home/pinodexmr/monero/build/release/bin/monerod.pid --ban-list /home/pinodexmr/block.txt --non-interactive
+./monerod --rpc-bind-ip=$DEVICE_IP --rpc-bind-port=$MONERO_PORT --confirm-external-bind --rpc-login=$RPCu:$RPCp --rpc-ssl disabled --in-peers=$IN_PEERS --out-peers=$OUT_PEERS --limit-rate-up=$LIMIT_RATE_UP --limit-rate-down=$LIMIT_RATE_DOWN --log-file=/var/www/html/monerod.log --max-log-file-size=10485000  --log-level=1 --max-log-files=1 --pidfile /home/pinodexmr/monero/build/release/bin/monerod.pid --enable-dns-blocklist --ban-list /home/pinodexmr/block.txt --non-interactive
