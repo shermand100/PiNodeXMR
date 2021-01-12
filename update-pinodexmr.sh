2c0cef7ea3b82a505e1ab26f77fa3903e409f070
shermand100 <shermand100@gmail.com>
2021-01-12 12:09:42 +0000

    Fix formatting for proper close of whiptail menu

--- update-pinodexmr.sh
+++ update-pinodexmr.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-					whiptail --title "PiNode-XMR Updater" --msgbox "\n\nPerforming PiNode-XMR update to latest version" 12 78
+					whiptail --title "PiNode-XMR Updater" --msgbox "\n\nPerforming PiNode-XMR update to latest version" 12 78;
 					
 ##Update and Upgrade system
 echo -e "\e[32mReceiving and applying Raspberry OS updates to latest versions\e[0m"
