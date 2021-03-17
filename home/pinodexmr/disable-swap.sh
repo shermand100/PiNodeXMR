c312b2a98b6b86936f7b756e4da30bc3d4330331
shermand100 <shermand100@gmail.com>
2021-03-17 19:13:44 +0000

    Make swap file configuration persistent over reboots

--- home/pinodexmr/disable-swap.sh
+++ home/pinodexmr/disable-swap.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 sudo dphys-swapfile swapoff
+sudo systemctl disable dphys-swapfile.service
 sleep 3
 free -h -w / > /var/www/html/free-h.txt
--- home/pinodexmr/enable-swap.sh
+++ home/pinodexmr/enable-swap.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 sudo dphys-swapfile swapon
+sudo systemctl enable dphys-swapfile.service
 sleep 3
 free -h -w / > /var/www/html/free-h.txt
