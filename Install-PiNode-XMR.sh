3b0ff86a309da5a9e7f2e030f0fe22efcbeb0d17
shermand100 <shermand100@gmail.com>
2021-10-08 15:16:04 +0100

    Update Install-PiNode-XMR.sh
    Update to add installer scripts for Armbian Bullseye OS release.
    Scripts added at merge https://github.com/monero-ecosystem/PiNode-XMR/commit/a45711c6c84b47b76299a8a48719d10641d3350a

--- Install-PiNode-XMR.sh
+++ Install-PiNode-XMR.sh
@@ -7,14 +7,15 @@ sudo apt-get install whiptail -y
 
 CHOICE=$(
 whiptail --title "Welcome to the PiNode-XMR Project" --menu "For correct installation select your OS" 20 60 5 \
-	"1)" "Raspberry Pi OS (Buster)"   \
-	"2)" "Armbian Debian (Buster)" \
-	"3)" "Exit"  3>&2 2>&1 1>&3	
+	"1)" "Raspberry Pi OS"   \
+	"2)" "Armbian Debian (Bullseye - latest)" \	
+	"3)" "Armbian Debian (Buster - older, stable)" \
+	"4)" "Exit"  3>&2 2>&1 1>&3	
 )
 
 case $CHOICE in
 	"1)")
-		#Commands for Raspbian
+		#Commands for Raspberry Pi OS
 		echo -e "\e[32mDownloading data for install\e[0m"
 		sleep 3
 		wget https://raw.githubusercontent.com/monero-ecosystem/PiNode-XMR/Raspbian-install/raspbian.sh
@@ -27,7 +28,20 @@ case $CHOICE in
 	;;
 
 	"2)")   
-		#Commands for Armbian
+		#Commands for Armbian Bullseye (latest)
+		echo -e "\e[32mDownloading data for install\e[0m"
+		sleep 3
+		wget https://raw.githubusercontent.com/monero-ecosystem/PiNode-XMR/Armbian-install/armbian-bullseye-installer.sh
+		echo -e "\e[32mPiNode-XMR Armbian configuration file received\e[0m"
+		echo -e "\e[32mStarting Installation\e[0m"
+		sudo chmod 755 ~/armbian-bullseye-installer.sh
+		sleep 2
+		./armbian-bullseye-installer.sh
+		exit 1
+        ;;	
+
+	"3)")   
+		#Commands for Armbian Buster (legacy stable)
 		echo -e "\e[32mDownloading data for install\e[0m"
 		sleep 3
 		wget https://raw.githubusercontent.com/monero-ecosystem/PiNode-XMR/Armbian-install/armbian-installer.sh
@@ -39,7 +53,7 @@ case $CHOICE in
 		exit 1
         ;;
 
-	"3)") exit
+	"4)") exit
         ;;
 esac
 exit
