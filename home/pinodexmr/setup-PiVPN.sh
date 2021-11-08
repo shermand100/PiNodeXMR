58593b3d48fc41961bfdf93040c2f5168c2b7894
shermand100 <shermand100@gmail.com>
2021-11-08 23:59:49 +0000

    PiVPN web path changed, add updater pre-install

--- home/pinodexmr/setup-PiVPN.sh
+++ home/pinodexmr/setup-PiVPN.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
+#Get latest sources and package updates
+sudo apt-get update && sudo apt-get upgrade -y
+
 #Configuration of Dynamic DNS service - instructions from https://www.noip.com/support/knowledgebase/installing-the-linux-dynamic-update-client/
 
-curl -L https://install.pivpn.dev | bash
+curl -L https://install.pivpn.io | bash
 
 whiptail --title "PiNode-XMR PiVPN config" --msgbox "PiVPN Installed.\n\nThe PiVPN is managed using the command line\nSee https://github.com/pivpn/pivpn for useage\n\nhit OK to return to main Menu" 8 78
 
