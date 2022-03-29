d3cb0a810ff5ead49820ffe5d7cd922a8f463248
monerooo <102408599+monerooo@users.noreply.github.com>
2022-03-29 17:07:43 +0200

    PHP fixes

--- HTML/i2p-address.php
+++ HTML/i2p-address.php
@@ -1,8 +1,7 @@
 <?php
 $VALUE = $_POST["value"];
 $fp = fopen('/home/pinodexmr/i2p-address.sh', 'w');
-fwrite($fp, "#!/bin/bash\nI2P_ADDRESS=$VALUE
-");
+fwrite($fp, "#!/bin/bash\nI2P_ADDRESS=$VALUE");
 fclose($fp);
 
 $fpa = fopen('/var/www/html/i2p-address.txt', 'w');
@@ -10,4 +9,4 @@ fwrite($fpa, "Your I2P address is set to: $VALUE");
 fclose($fpa);
 
 echo "Your I2P address has been set to: $VALUE ";
- ?>
+?>
--- HTML/monerod-start-i2p.php
+++ HTML/monerod-start-i2p.php
@@ -1,4 +1,4 @@
 <?php 
   exec("sudo systemctl start monerod-start-i2p.service");
   echo "Start Command Sent to bridge transaction broadcast through I2P";
- ?>
+?>
