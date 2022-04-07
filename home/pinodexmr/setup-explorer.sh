cbc6a958e6b60451d553dde084e248cc3453d538
shermand100 <shermand100@gmail.com>
2022-04-07 11:56:31 +0100

    Temp remove block explorer

--- home/pinodexmr/setup-explorer.sh
+++ home/pinodexmr/setup-explorer.sh
@@ -1,30 +1,32 @@
-#!/bin/bash
-		CHOICE=$(whiptail --title "Monero Onion Blockchain Explorer" --menu "\n\nStart or Stop the Blockchain Explorer manually" 20 60 10 \
-	"1)" "Start Blockchain Explorer"   \
-	"2)" "Stop Blockchain Explorer"   \
-	"3)" "Cancel" 2>&1 >/dev/tty)
+!/bin/bash
+# 		CHOICE=$(whiptail --title "Monero Onion Blockchain Explorer" --menu "\n\nStart or Stop the Blockchain Explorer manually" 20 60 10 \
+# 	"1)" "Start Blockchain Explorer"   \
+# 	"2)" "Stop Blockchain Explorer"   \
+# 	"3)" "Cancel" 2>&1 >/dev/tty)
 
-case $CHOICE in
-		"1)") sudo systemctl start explorer-start.service
-			TERM=vt220 whiptail --infobox "Please Wait...\n\nCommand has been passed to systemd to start the Monero Onion Blockchain Explorer" 12 78
-			echo "#!/bin/sh
-EXPLORER_START=1" > /home/pinodexmr/explorer-flag.sh
-			whiptail --title "Monero Onion Blockchain Explorer" --msgbox "Systemd has successfully started the Monero Onion Blockchain Explorer" 12 78
-			clear
-			. /home/pinodexmr/setup.sh
+# case $CHOICE in
+# 		"1)") sudo systemctl start explorer-start.service
+# 			TERM=vt220 whiptail --infobox "Please Wait...\n\nCommand has been passed to systemd to start the Monero Onion Blockchain Explorer" 12 78
+# 			whiptail --title "Monero Onion Blockchain Explorer" --msgbox "Systemd has successfully started the Monero Onion Blockchain Explorer" 12 78
+#             sudo systemctl enable explorer-start.service
+# 			clear
+# 			. /home/pinodexmr/setup.sh
 
-            ;;
+#             ;;
 
-		"2)") sudo systemctl stop explorer-start.service
-			TERM=vt220 whiptail --infobox "Please Wait...\n\nCommand has been passed to systemd to stop the Monero Onion Blockchain Explorer" 12 78
-			echo "#!/bin/sh
-EXPLORER_START=0" > /home/pinodexmr/explorer-flag.sh
-			whiptail --title "Monero Onion Blockchain Explorer" --msgbox "Systemd has successfully stopped the Monero Onion Blockchain Explorer" 12 78
-			clear
-			. /home/pinodexmr/setup.sh
-			;;
+# 		"2)") sudo systemctl stop explorer-start.service
+# 			TERM=vt220 whiptail --infobox "Please Wait...\n\nCommand has been passed to systemd to stop the Monero Onion Blockchain Explorer" 12 78
+# 			whiptail --title "Monero Onion Blockchain Explorer" --msgbox "Systemd has successfully stopped the Monero Onion Blockchain Explorer" 12 78
+#             sudo systemctl disable explorer-start.service
+# 			clear
+# 			. /home/pinodexmr/setup.sh
+# 			;;
 		
-		"3)") . /home/pinodexmr/setup.sh
-			;;
+# 		"3)") . /home/pinodexmr/setup.sh
+# 			;;
 		
-esac
+# esac
+
+whiptail --title "Monero Onion Blockchain Explorer" --msgbox "Due to stability issues the block explorer has been disabled, hopefully as a temporary measure. I hope to return it to this project in the future." 20 78
+		
+. /home/pinodexmr/setup.sh
