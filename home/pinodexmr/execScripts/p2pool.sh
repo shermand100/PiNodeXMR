96a5735d7765596428262a7acd21bba77bbfd8d4
shermand100 <shermand100@gmail.com>
2022-04-20 20:50:15 +0100

    P2Pool corrct variables path. no colour in log, light mode to save RAM

--- home/pinodexmr/execScripts/p2pool.sh
+++ home/pinodexmr/execScripts/p2pool.sh
@@ -2,12 +2,12 @@
 
 #Import Start Flag Values:
 	#Establish Device IP
-	. /home/pinodexmr/deviceIp.sh
+	. /home/pinodexmr/variables/deviceIp.sh
 	#Import Port Number
-	. /home/pinodexmr/monero-port.sh
+	. /home/pinodexmr/variables/monero-port.sh
 	#Import Mining Address
-	. /home/pinodexmr/mining-address.sh	
+	. /home/pinodexmr/variables/mining-address.sh	
 
 #Start P2Pool
 cd /home/pinodexmr/p2pool/build
-./p2pool --host $DEVICE_IP --wallet $MINING_ADDRESS
+./p2pool --host $DEVICE_IP --wallet $MINING_ADDRESS --no-color --light-mode
