0943b2d5d13c20e6e626aecd25b0bb708d2ba385
shermand100 <shermand100@gmail.com>
2022-05-13 12:27:14 +0100

    Fix Setup usb path

--- home/pinodexmr/setupMenuScripts/setup-usb-select-device.sh
+++ home/pinodexmr/setupMenuScripts/setup-usb-select-device.sh
@@ -32,49 +32,49 @@ LINE8=$(lsblk --nodeps | sed -n 9p)
 		
 		"1)") DEVICE_TO_CONFIGURE=$(lsblk --nodeps -o name | sed -n 2p)
 		echo "#!/bin/sh
-DEVICE_TO_CONFIGURE=$(lsblk --nodeps -o name | sed -n 2p)" > /home/pinodexmr/setup-usb-path.sh
+DEVICE_TO_CONFIGURE=$(lsblk --nodeps -o name | sed -n 2p)" > /home/pinodexmr/setupMenuScripts/setup-usb-path.sh
 				sudo /home/pinodexmr/setupMenuScripts/setup-usb.sh /dev/$DEVICE_TO_CONFIGURE XMRBLOCKCHAIN
 		;;
 				
 		"2)")DEVICE_TO_CONFIGURE=$(lsblk --nodeps -o name | sed -n 3p)
 		echo "#!/bin/sh
-DEVICE_TO_CONFIGURE=$(lsblk --nodeps -o name | sed -n 3p)" > /home/pinodexmr/setup-usb-path.sh
+DEVICE_TO_CONFIGURE=$(lsblk --nodeps -o name | sed -n 3p)" > /home/pinodexmr/setupMenuScripts/setup-usb-path.sh
 				sudo /home/pinodexmr/setupMenuScripts/setup-usb.sh /dev/$DEVICE_TO_CONFIGURE XMRBLOCKCHAIN
 		;;
 		
 		"3)")DEVICE_TO_CONFIGURE=$(lsblk --nodeps -o name | sed -n 4p)
 		echo "#!/bin/sh
-DEVICE_TO_CONFIGURE=$(lsblk --nodeps -o name | sed -n 4p)" > /home/pinodexmr/setup-usb-path.sh
+DEVICE_TO_CONFIGURE=$(lsblk --nodeps -o name | sed -n 4p)" > /home/pinodexmr/setupMenuScripts/setup-usb-path.sh
 				sudo /home/pinodexmr/setupMenuScripts/setup-usb.sh /dev/$DEVICE_TO_CONFIGURE XMRBLOCKCHAIN
 		;;
 		
 		"4)")DEVICE_TO_CONFIGURE=$(lsblk --nodeps -o name | sed -n 5p)
 		echo "#!/bin/sh
-DEVICE_TO_CONFIGURE=$(lsblk --nodeps -o name | sed -n 5p)" > /home/pinodexmr/setup-usb-path.sh
+DEVICE_TO_CONFIGURE=$(lsblk --nodeps -o name | sed -n 5p)" > /home/pinodexmr/setupMenuScripts/setup-usb-path.sh
 				sudo /home/pinodexmr/setupMenuScripts/setup-usb.sh /dev/$DEVICE_TO_CONFIGURE XMRBLOCKCHAIN
 		;;
 
 		"5)")DEVICE_TO_CONFIGURE=$(lsblk --nodeps -o name | sed -n 6p)
 		echo "#!/bin/sh
-DEVICE_TO_CONFIGURE=$(lsblk --nodeps -o name | sed -n 6p)" > /home/pinodexmr/setup-usb-path.sh
+DEVICE_TO_CONFIGURE=$(lsblk --nodeps -o name | sed -n 6p)" > /home/pinodexmr/setupMenuScripts/setup-usb-path.sh
 				sudo /home/pinodexmr/setupMenuScripts/setup-usb.sh /dev/$DEVICE_TO_CONFIGURE XMRBLOCKCHAIN
 		;;
 
 		"6)")DEVICE_TO_CONFIGURE=$(lsblk --nodeps -o name | sed -n 7p)
 		echo "#!/bin/sh
-DEVICE_TO_CONFIGURE=$(lsblk --nodeps -o name | sed -n 7p)" > /home/pinodexmr/setup-usb-path.sh
+DEVICE_TO_CONFIGURE=$(lsblk --nodeps -o name | sed -n 7p)" > /home/pinodexmr/setupMenuScripts/setup-usb-path.sh
 				sudo /home/pinodexmr/setupMenuScripts/setup-usb.sh /dev/$DEVICE_TO_CONFIGURE XMRBLOCKCHAIN
 		;;
 		
 		"7)")DEVICE_TO_CONFIGURE=$(lsblk --nodeps -o name | sed -n 8p)
 		echo "#!/bin/sh
-DEVICE_TO_CONFIGURE=$(lsblk --nodeps -o name | sed -n 8p)" > /home/pinodexmr/setup-usb-path.sh
+DEVICE_TO_CONFIGURE=$(lsblk --nodeps -o name | sed -n 8p)" > /home/pinodexmr/setupMenuScripts/setup-usb-path.sh
 				sudo /home/pinodexmr/setupMenuScripts/setup-usb.sh /dev/$DEVICE_TO_CONFIGURE XMRBLOCKCHAIN
 		;;
 		
 		"8)")DEVICE_TO_CONFIGURE=$(lsblk --nodeps -o name | sed -n 9p)
 		echo "#!/bin/sh
-DEVICE_TO_CONFIGURE=$(lsblk --nodeps -o name | sed -n 9p)" > /home/pinodexmr/setup-usb-path.sh
+DEVICE_TO_CONFIGURE=$(lsblk --nodeps -o name | sed -n 9p)" > /home/pinodexmr/setupMenuScripts/setup-usb-path.sh
 				sudo /home/pinodexmr/setupMenuScripts/setup-usb.sh /dev/$DEVICE_TO_CONFIGURE XMRBLOCKCHAIN
 		;;
 	esac
