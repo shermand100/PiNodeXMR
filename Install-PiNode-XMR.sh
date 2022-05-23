ecdf2cce854c4f26b06cf1d7196f94d2e09dfe65
shermand100 <shermand100@gmail.com>
2022-05-23 19:17:58 +0100

    Fix improper placement of "

--- Install-PiNode-XMR.sh
+++ Install-PiNode-XMR.sh
@@ -6,7 +6,7 @@ sleep 2
 sudo apt-get install whiptail -y
 
 CHOICE=$(whiptail --title "Welcome to the PiNode-XMR Project" --menu "For correct installation select your OS" 20 60 5 \
-	"1)" "Ubunutu Server 64bit 22.04 LTS" (current) \
+	"1)" "Ubunutu Server 64bit 22.04 LTS (current)" \
 	"2)" "Armbian Debian (Bullseye - discontinued)" \
 	"3)" "Armbian Debian (Buster - end of life)" \
 	"4)" "Raspberry Pi OS (end of life)" \
