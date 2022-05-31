cf9562def057cb1eb239e6df3ff715d005dd959b
Devin <devin@blackhat.co.za>
2022-05-31 16:12:08 +0200

    consolidated scripts to single file to make upkeep easier

--- HTML/stop-moneroMiningNode.php
+++
@@ -1,6 +0,0 @@
-<?php
-  exec("sudo systemctl stop moneroMiningNode.service");
-  exec("sudo systemctl disable moneroMiningNode.service");
-  echo "Stop Command Sent for Monero Mining Node";
-  exec (". /home/pinodexmr/bootStatusSetIdle.sh");
-?>
