88761277c780792db9ccfdf3a408dddec0a35a0d
shermand100 <shermand100@gmail.com>
2022-06-12 17:00:25 +0100

    Dependency "libboost-all-dev" Missing

--- home/pinodexmr/setupMenuScripts/setup-update-monero.sh
+++ home/pinodexmr/setupMenuScripts/setup-update-monero.sh
@@ -101,6 +101,11 @@ sudo cmake . 2>&1 | tee -a debug.log
 sudo make
 sudo mv lib/libg* /usr/lib/
 cd
+echo "Check dependencies installed for --- Monero" 2>&1 | tee -a debug.log
+sudo apt-get update
+sudo apt-get install build-essential cmake pkg-config libssl-dev libzmq3-dev libunbound-dev libsodium-dev libunwind8-dev liblzma-dev libreadline6-dev libldns-dev libexpat1-dev libpgm-dev qttools5-dev-tools libhidapi-dev libusb-1.0-0-dev libprotobuf-dev protobuf-compiler libudev-dev libboost-chrono-dev libboost-date-time-dev libboost-filesystem-dev libboost-locale-dev libboost-program-options-dev libboost-regex-dev libboost-all-dev libboost-serialization-dev libboost-system-dev libboost-thread-dev ccache doxygen graphviz -y 2>&1 | tee -a debug.log
+
+
 echo -e "\e[32mDownloading Monero \e[0m"
 sleep 2
 
--- ubuntu-install-continue.sh
+++ ubuntu-install-continue.sh
@@ -114,7 +114,7 @@ then
 echo -e "\e[32mInstalling dependencies for --- Monero\e[0m"
 sleep 3
 sudo apt-get update
-sudo apt-get install build-essential cmake pkg-config libssl-dev libzmq3-dev libunbound-dev libsodium-dev libunwind8-dev liblzma-dev libreadline6-dev libldns-dev libexpat1-dev libpgm-dev qttools5-dev-tools libhidapi-dev libusb-1.0-0-dev libprotobuf-dev protobuf-compiler libudev-dev libboost-chrono-dev libboost-date-time-dev libboost-filesystem-dev libboost-locale-dev libboost-program-options-dev libboost-regex-dev libboost-serialization-dev libboost-system-dev libboost-thread-dev ccache doxygen graphviz -y 2>&1 | tee -a debug.log
+sudo apt-get install build-essential cmake pkg-config libssl-dev libzmq3-dev libunbound-dev libsodium-dev libunwind8-dev liblzma-dev libreadline6-dev libldns-dev libexpat1-dev libpgm-dev qttools5-dev-tools libhidapi-dev libusb-1.0-0-dev libprotobuf-dev protobuf-compiler libudev-dev libboost-chrono-dev libboost-date-time-dev libboost-filesystem-dev libboost-locale-dev libboost-program-options-dev libboost-regex-dev libboost-all-dev libboost-serialization-dev libboost-system-dev libboost-thread-dev ccache doxygen graphviz -y 2>&1 | tee -a debug.log
 sleep 2
 	echo "manual build of gtest for --- Monero" 2>&1 | tee -a debug.log
 sudo apt-get install libgtest-dev -y 2>&1 | tee -a debug.log
