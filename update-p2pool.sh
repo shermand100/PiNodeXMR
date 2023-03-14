3e8a606ff960a640ba4c492d4d354fdd0c76ccfa
intr <15019654+intr-cx@users.noreply.github.com>
2023-03-14 15:14:18 +0059

    Tidy update-p2pool

--- update-p2pool.sh
+++ update-p2pool.sh
@@ -1,47 +1,47 @@
 #!/bin/bash
 
-echo "
-####################
-" 2>&1 | tee -a /home/nanode/debug.log
-echo "Start setup-update-p2pool.sh script $(date)" 2>&1 | tee -a /home/nanode/debug.log
-echo "
-####################
-" 2>&1 | tee -a /home/nanode/debug.log
+. ./common.sh
 
+showtext "
+####################
+Start setup-update-p2pool.sh script $(date)
+####################
+"
 
 #Stop Node to make system resources available.
-sudo systemctl stop blockExplorer.service
-sudo systemctl stop moneroPrivate.service
-sudo systemctl stop moneroMiningNode.service
-sudo systemctl stop moneroTorPrivate.service
-sudo systemctl stop moneroTorPublic.service
-sudo systemctl stop moneroPublicFree.service
-sudo systemctl stop moneroI2PPrivate.service
-sudo systemctl stop moneroCustomNode.service
-sudo systemctl stop moneroPublicRPCPay.service
-sudo systemctl stop p2pool.service
+sudo systemctl stop blockExplorer.service \
+	moneroPrivate.service \
+	moneroMiningNode.service \
+	moneroTorPrivate.service \
+	moneroTorPublic.service \
+	moneroPublicFree.service \
+	moneroI2PPrivate.service \
+	moneroCustomNode.service \
+	moneroPublicRPCPay.service \
+	p2pool.service
+
 echo "Monero node stop command sent to make system resources available for update, allowing 30 seconds for safe shutdown"
-echo "Update starts in 20 seconds"
-echo "Update starts in 10 seconds"
-echo "Update starts in 5 seconds"
-echo -e "\e[32mDelete old version\e[0m"
+sleep 30
+showtext "Deleting old version"
 rm -rf /home/nanode/p2pool/
-echo -e "\e[32mSuccess\e[0m"
-echo -e "\e[32mBuilding new P2Pool\e[0m"
+showtext "Building new P2Pool"
 ##Install P2Pool
-git clone --recursive https://github.com/SChernykh/p2pool 2>&1 | tee -a /home/nanode/debug.log
-cd p2pool
-git checkout tags/v3.0
-mkdir build && cd build
-cmake .. 2>&1 | tee -a /home/nanode/debug.log
-make -j2 2>&1 | tee -a /home/nanode/debug.log
-echo -e "\e[32mSuccess\e[0m"
-cd
-#Update system reference Explorer version number version number
-chmod 755 /home/nanode/p2pool-new-ver.sh
-. /home/nanode/p2pool-new-ver.sh
-echo "#!/bin/bash
-CURRENT_VERSION_P2POOL=$NEW_VERSION_P2POOL" > /home/nanode/current-ver-p2pool.sh 2>&1 | tee -a /home/nanode/debug.log
+{
+	git clone --recursive https://github.com/SChernykh/p2pool
+	cd p2pool || exit 1
+	git checkout tags/v3.0
+	mkdir build && (cd build || exit 1)
+	cmake ..
+	make -j2
+	showtext "Success"
+	cd || exit 1
+	#Update system reference Explorer version number version number
+	chmod 755 /home/nanode/p2pool-new-ver.sh
+	#shellcheck disable=SC1091
+	. /home/nanode/p2pool-new-ver.sh
+	echo "#!/bin/bash
+	CURRENT_VERSION_P2POOL=$NEW_VERSION_P2POOL" > /home/nanode/current-ver-p2pool.sh
+} 2>&1 | tee -a "$DEBUG_LOG"
 
 #Define Restart Monero Node
 # Key - BOOT_STATUS
@@ -52,55 +52,40 @@ CURRENT_VERSION_P2POOL=$NEW_VERSION_P2POOL" > /home/nanode/current-ver-p2pool.sh
 # 7 = Public free
 # 8 = I2P
 # 9 tor public
-if [[ $BOOT_STATUS -eq 2 ]]
+if [ "$BOOT_STATUS" -eq 2 ]
 then
-	whiptail --title "P2Pool Update Complete" --msgbox "Update complete, Node ready for start. See web-ui at $(hostname -I) to select mode." 16 60
+	whiptail --title "Monero Update Complete" --msgbox "Update complete, Node ready for start. See web-ui at $(hostname -I) to select mode." 16 60
+else
+	case $BOOT_STATUS in
+		3)
+			sudo systemctl start moneroPrivate.service
+			;;
+		4)
+			sudo systemctl start moneroTorPrivate.service
+			;;
+		# 5) TODO apparently not needed
+			# 	sudo systemctl start moneroMiningNode.service
+			# 	;;
+		6)
+			sudo systemctl start moneroPublicRPCPay.service
+			;;
+		7)
+			sudo systemctl start moneroPublicFree.service
+			;;
+		8)
+			sudo systemctl start moneroI2PPrivate.service
+			;;
+		9)
+			sudo systemctl start moneroTorPublic.service
+			;;
+		*)
+			log "Very strange"
+			;;
+	esac
+	whiptail --title "Monero Update Complete" --msgbox "Update complete, Your Monero Node has resumed." 16 60
 fi
-
-if [[ $BOOT_STATUS -eq 3 ]]
-then
-	sudo systemctl start moneroPrivate.service
-	whiptail --title "P2Pool Update Complete" --msgbox "Update complete, Your Monero Node has resumed." 16 60
-fi
-
-if [[ $BOOT_STATUS -eq 4 ]]
-then
-	sudo systemctl start moneroTorPrivate.service
-	whiptail --title "P2Pool Update Complete" --msgbox "Update complete, Your Monero Node has resumed." 16 60
-fi
-
-if [[ $BOOT_STATUS -eq 5 ]]
-then
-	sudo systemctl start moneroMiningNode.service
-	whiptail --title "P2Pool Update Complete" --msgbox "Update complete, Your Monero Node has resumed." 16 60
-fi
-
-if [[ $BOOT_STATUS -eq 6 ]]
-then
-	sudo systemctl start moneroPublicRPCPay.service
-	whiptail --title "P2Pool Update Complete" --msgbox "Update complete, Your Monero Node has resumed." 16 60
-fi
-
-if [[ $BOOT_STATUS -eq 7 ]]
-then
-	sudo systemctl start moneroPublicFree.service
-	whiptail --title "P2Pool Update Complete" --msgbox "Update complete, Your Monero Node has resumed." 16 60
-fi
-
-if [[ $BOOT_STATUS -eq 8 ]]
-then
-	sudo systemctl start moneroI2PPrivate.service
-	whiptail --title "P2Pool Update Complete" --msgbox "Update complete, Your Monero Node has resumed." 16 60
-fi
-
-if [[ $BOOT_STATUS -eq 9 ]]
-then
-	sudo systemctl start moneroTorPublic.service
-	whiptail --title "P2Pool Update Complete" --msgbox "Update complete, Your Monero Node has resumed." 16 60
-fi
-
-	##End debug log
-	echo "Update Script Complete" 2>&1 | tee -a /home/nanode/debug.log
-	echo "####################" 2>&1 | tee -a /home/nanode/debug.log
-	echo "End setup-update-p2pool.sh script $(date)" 2>&1 | tee -a /home/nanode/debug.log
-	echo "####################" 2>&1 | tee -a /home/nanode/debug.log
+##End debug log
+log "Update Script Complete
+####################
+End setup-update-p2pool.sh script $(date)
+####################"
