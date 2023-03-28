aa0d0ca5642405290088bdb5e62884d9d3d8e2a4
intr <15019654+intr-cx@users.noreply.github.com>
2023-03-28 16:17:20 +0200

    Fix explorer build & run

--- home/nanode/execScripts/blockExplorer.sh
+++ home/nanode/execScripts/blockExplorer.sh
@@ -18,17 +18,14 @@ RPCp=$(getvar "rpcp")
 if [ "$BOOT_STATUS" -eq 6 ]
 then
 	#Adapted command for starting onion-block-explorer only for public node due to restricted rpc commands
-	cd /home/nanode/onion-monero-blockchain-explorer/build/ || exit 1
-	./xmrblocks --port 8081 --enable-pusher --enable-emission-monitor --daemon-url="HTTP://${DEVICE_IP// }:$MONERO_PORT" --mempool-info-timeout 60000 --mempool-refresh-time 30 --concurrency 1
+	/usr/bin/xmrblocks --port 8081 --enable-pusher --enable-emission-monitor --daemon-url="HTTP://${DEVICE_IP// }:$MONERO_PORT" --mempool-info-timeout 60000 --mempool-refresh-time 30 --concurrency 1
 else
 	if [ "$BOOT_STATUS" -eq 7 ]
 	then
 		#Adapted command for starting onion-block-explorer only for public node due to restricted rpc commands
-		cd /home/nanode/onion-monero-blockchain-explorer/build/ || exit 1
-		./xmrblocks --port 8081 --enable-pusher=1 --enable-emission-monitor=1 --daemon-url="HTTP://${DEVICE_IP// }:$MONERO_PUBLIC_PORT" --concurrency=1
+		/usr/bin/xmrblocks --port 8081 --enable-pusher=1 --enable-emission-monitor=1 --daemon-url="HTTP://${DEVICE_IP// }:$MONERO_PUBLIC_PORT" --concurrency=1
 	else
 		#Start onion-block-explorer
-		cd /home/nanode/onion-monero-blockchain-explorer/build/ || exit 1
-		./xmrblocks --port 8081 --enable-pusher --enable-emission-monitor --daemon-url="HTTP://${DEVICE_IP// }:$MONERO_PORT" --daemon-login "$RPCu:$RPCp" --mempool-info-timeout 60000 --mempool-refresh-time 30 --concurrency 1
+		/usr/bin/xmrblocks --port 8081 --enable-pusher --enable-emission-monitor --daemon-url="HTTP://${DEVICE_IP// }:$MONERO_PORT" --daemon-login "$RPCu:$RPCp" --mempool-info-timeout 60000 --mempool-refresh-time 30 --concurrency 1
 	fi
 fi
--- update-explorer.sh
+++ update-explorer.sh
@@ -15,30 +15,26 @@ Start setup-update-explorer.sh script $(date)
 
 #(1) Define variables and updater functions
 
-{
-	#Permission Setting
-	chmod 755 /home/nanode/current-ver-exp.sh
-	chmod 755 /home/nanode/exp-new-ver.sh
-} 2>&1 | tee -a "$DEBUG_LOG"
-
-
 #Stop Node to make system resources available.
-{
 services-stop
-sudo systemctl stop blockExplorer.service
+systemctl stop blockExplorer.service
 rm -rf /home/nanode/onion-monero-blockchain-explorer/
 showtext "Building Monero Blockchain Explorer
 *******************************************************
 ***This will take a few minutes - Hardware Dependent***
 *******************************************************"
-git clone -b master https://github.com/moneroexamples/onion-monero-blockchain-explorer.git
-cd onion-monero-blockchain-explorer || exit
-mkdir build && cd build || exit
-cmake ..
-make
+
+{
+	git clone -b master https://github.com/moneroexamples/onion-monero-blockchain-explorer.git
+	cd onion-monero-blockchain-explorer || exit
+	mkdir build
+	cd build || exit
+	cmake ..
+	make && cp xmrblocks /usr/bin/ && chmod a+x /usr/bin/xmrblocks
+} 2>&1 | tee -a "$DEBUG_LOG"
+
 putvar "versions.exp" "$NEW_VERSION_EXP"
 services-start
-} 2>&1 | tee -a "$DEBUG_LOG"
 #
 ##End debug log
 echo "
