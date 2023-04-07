169aeb3f77eb27141d177eaac5ef3a74861c48b9
Abdullah Khan <abdullahdostkhan@gmail.com>
2023-04-07 20:43:49 +0530

    Fixing showtext in update-monero-lws.sh

--- update-monero-lws.sh
+++ update-monero-lws.sh
@@ -13,23 +13,23 @@ Start setup-update-monero-lws.sh script $(date)
 "
 
 #Stop Monero-lws service (if started)
-showtext "Stop Monero-lws service (if started"
+showtext "Stopping Monero-LWS service (if started)..."
 sudo systemctl stop monero-lws.service
 ##Delete old version
-showtext "Delete old versio"
+showtext "Deleting old version..."
 rm -rf /home/nanode/monero-lws/ 2>&1 | tee -a "$DEBUG_LOG"
-showtext "Downloading and Building new Monero-LW"
+showtext "Checking dependancies..."
 #Check dependencies (Should be installed already from Monero install)
 sudo apt-get update
 sudo apt-get install build-essential cmake libboost-all-dev libssl-dev libzmq3-dev doxygen graphviz -y -o DPkg::Options::="--force-confnew" 2>&1 | tee -a "$DEBUG_LOG"
-showtext "Downloading VTNerd Monero-LW"
+showtext "Downloading Monero-LWS..."
 {
 	git clone --recursive https://github.com/vtnerd/monero-lws.git
 	cd monero-lws || exit 1
 	git checkout release-v0.2_0.18
 	mkdir build && cd build || exit 1
 	cmake -DMONERO_SOURCE_DIR=/root/monero -DMONERO_BUILD_DIR=/root/monero/build/release ..
-	showtext "Building VTNerd Monero-LW"
+	showtext "Building Monero-LWS..."
 	make
 } 2>&1 | tee -a "$DEBUG_LOG"
 cd || exit 1
@@ -39,7 +39,7 @@ sudo systemctl start monero-lws.service
 putvar "versions.lws" "$NEW_VERSION_LWS"
 
 ##End debug log
-showtext "Update Complete
+showtext "Monero-LWS Updated"
 ####################
 End setup-update-monero-lws.sh script $(date)
 ####################"
