fccef8729a4c0ef8b61a69563bd48991d971ee27
intr <15019654+intr-cx@users.noreply.github.com>
2023-04-16 13:31:29 +0200

    Grep regex does not like \d

--- setup-drive.sh
+++ setup-drive.sh
@@ -6,7 +6,7 @@
 showtext "Formatting SSD..."
 {
 	#Narrows down non-rotational drives with nvme0n1 blockdevice, picks the top one. Hacky but should work.
-	device=$(lsblk -o KNAME,VENDOR,ROTA | grep -e "0$" | grep -e "nvme\dn\d" | awk '{print $1}' | head -n1)
+	device=$(lsblk -o KNAME,VENDOR,ROTA | grep -e "0$" | grep -e "nvme[:digit:]n[:digit:]" | awk '{print $1}' | head -n1)
 	showtext "Using device $device."
 	if [ -n "$device" ]; then
 		setup_drive "$device" xfs
