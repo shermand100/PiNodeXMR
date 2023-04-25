87ef229521d2fd8679143428e1556ab2b8590c3b
intr <15019654+intr-cx@users.noreply.github.com>
2023-04-25 13:23:22 +0200

    Set lws-admin servers

--- home/nodo/execScripts/monero-lws.sh
+++ home/nodo/execScripts/monero-lws.sh
@@ -7,4 +7,4 @@ lwsloc=/home/nodo/monero-lws/build/src/
 
 DATA_DIR=$(getvar "data_dir")/light_wallet_server
 
-eval "${lwsloc}monero-lws-daemon --daemon tcp://127.0.0.1:18083 --db-path $DATA_DIR"
+eval "${lwsloc}monero-lws-daemon --daemon tcp://127.0.0.1:18083 --db-path $DATA_DIR --admin-rest-server https://127.0.0.1:8443/admin --rest-server https://127.0.0.1:8443/basic"
