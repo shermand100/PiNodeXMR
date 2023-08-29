d5e8df409b8ff7547c13c12ff513c9c8009fa897
shermand100 <shermand100@gmail.com>
2023-08-29 17:41:18 +0100

    Start of Atomic Swap "swapd" uses internal Monero Node and detailed log-level debug to help solve future bugs

--- home/pinodexmr/execScripts/atomicSwap.sh
+++ home/pinodexmr/execScripts/atomicSwap.sh
@@ -3,7 +3,11 @@
 #Import Start Flag Values:
 	#Import Ethereum JSON RPC Endpoint
 	. /home/pinodexmr/variables/eth-rpc-node.sh
+	#Establish Device IP
+	. /home/pinodexmr/variables/deviceIp.sh
+	#Import Monero RPC Port Number
+	. /home/pinodexmr/variables/monero-port.sh
 
 #Start Atomic Swap
-cd /home/pinodexmr/bin
-./swapd --env mainnet --eth-endpoint $ETH_RPC_NODE
+
+~/atomic-swap/bin/swapd --env mainnet --eth-endpoint $ETH_RPC_NODE --monerod-host $DEVICE_IP --monerod-port $MONERO_PORT --log-level debug
