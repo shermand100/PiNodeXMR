0914bf968e64794a0de9221e2120e5de1c2e0482
shermand100 <shermand100@gmail.com>
2024-02-02 12:09:13 +0000

    Trigger support. RPi4 stack errors. Install pre-compiled Monerod

--- supportScript1.sh
+++ supportScript1.sh
@@ -11,16 +11,27 @@
 
 ###
 #Start Default action below - no actions performed (Comment out as required)
-whiptail --title "PiNode-XMR Support Script #1" --msgbox "This script has not been configured to provide support at this time\n\nNo Actions performed\n\nReturning to Menu" 12 78;
+# whiptail --title "PiNode-XMR Support Script #1" --msgbox "This script has not been configured to provide support at this time\n\nNo Actions performed\n\nReturning to Menu" 12 78;
 #End Default action 
 ###
 
 ###
 #Start Assist User
-# if (whiptail --title "PiNode-XMR Support Script #1" --yesno "This script will now perform actions on your device configured exclusively for the benefit of user: ### \n\nIf you are not intended user: ### these actions are likely to affect your device negatively\n\nWould you like to continue?" 12 78); then
-#     #Insert required support/customisation commands here
-# else
-#     sleep 2
-# fi
+if (whiptail --title "PiNode-XMR Support Script #1" --yesno "This script will now perform actions on your device configured exclusively for the benefit of user: TheB*** and JJ \n\nThis script will download and install the pre-compiled Monerod to assist RPi4 users\n\nWould you like to continue?" 12 78); then
+#Remove current monero installation
+rm -r /home/pinodexmr/monero/build/release/bin/*
+#Download Monero Arm
+wget https://downloads.getmonero.org/cli/linuxarm8
+#Make temp folder to extract binaries
+mkdir temp && tar -xvf linuxarm8 -C ~/temp
+#Move Monerod files to standard location
+mv /home/pinodexmr/temp/monero-aarch64-linux-gnu-v0.18*/monero* /home/pinodexmr/monero/build/release/bin/
+rm linuxarm8
+rm -R /home/pinodexmr/temp/
+whiptail --title "Install complete" --msgbox "Monero pre-compiled Arm8 binaries have been downloaded and installed \n\nSelect ok to return to menu" 16 60
+
+else
+    sleep 2
+fi
 #End Assist User
 ###
