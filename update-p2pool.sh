6f6df6ab7fbb10e316b933b920696bc0783f114e
shermand100 <shermand100@gmail.com>
2025-06-17 10:28:45 +0100

    P2Pool v4.8

--- update-p2pool.sh
+++ update-p2pool.sh
@@ -68,7 +68,7 @@ fi
 		##Install P2Pool
 		git clone --recursive https://github.com/SChernykh/p2pool 2>&1 | tee -a /home/pinodexmr/debug.log
 		cd p2pool
-		git checkout tags/v4.7
+		git checkout tags/v4.8
 		mkdir build && cd build
 
 #Implement merge minign donation choice
