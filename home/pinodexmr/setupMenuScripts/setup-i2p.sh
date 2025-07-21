01bf5e53fd0a906f8baf75103cbac5f4f12fde45
shermand100 <shermand100@gmail.com>
2025-07-21 11:08:41 +0100

    Use simple config suitable for Ubuntu Server
    Method chosen uses Java based I2P (as opposed to I2Pd C++ method ) due to built in web interface packaged as standard. This keep user interface graphical after terminal based install for simplicity and PiNodeXMR/Monero setup standard as per documented:
    
    https://github.com/shermand100/PiNodeXMR/wiki/i2p-Setup-and-Use

--- home/pinodexmr/setupMenuScripts/setup-i2p.sh
+++ home/pinodexmr/setupMenuScripts/setup-i2p.sh
@@ -26,24 +26,7 @@ sleep 2
 i2prouter start
 
 sleep 5
-
-###Make web console accessable on lan
-
-	##Set Device IP variable
-	cd
-	. ~/variables/deviceIp.sh
-
-	##add lan ip to config (default blocks lan access to web client)
-	#This allows access from ip e.g.: 192.168.1.116:7657
-	sed -i "/clientApp.0.args*/c\clientApp.0.args=7657 ::1,127.0.0.1,"${DEVICE_IP}" ./webapps/" /home/pinodexmr/.i2p/clients.config.d/00-net.i2p.router.web.RouterConsoleRunner-clients.config
-	#This allows access from hostname e.g: http://pinodexmr.local:7657
-	echo "routerconsole.allowedHosts=pinodexmr.local" >> /home/pinodexmr/.i2p/router.config
-
-sleep 2 
-    ##Apply changes
-    i2prouter restart
-
-
+						
 whiptail --title "PiNode-XMR I2P" --msgbox "I2P server has been installed and started\n\nYou now have access to the I2P config menu found at $(hostname -I | awk '{print $1}'):7657" 12 78
 
 
