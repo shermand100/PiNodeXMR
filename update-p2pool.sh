726e5f01c38ca0f053f0b87cff6f64f5ed7dbf22
shermand100 <shermand100@gmail.com>
2025-08-04 19:34:47 +0100

    Fix P2Pool build v4.9

--- update-p2pool.sh
+++ update-p2pool.sh
@@ -66,9 +66,8 @@ fi
 		sleep "2"
 		echo -e "\e[32mBuilding new P2Pool\e[0m"
 		##Install P2Pool
-		git clone --recursive https://github.com/SChernykh/p2pool 2>&1 | tee -a /home/pinodexmr/debug.log
+		git clone --recursive --branch v4.9 https://github.com/SChernykh/p2pool 2>&1 | tee -a /home/pinodexmr/debug.log
 		cd p2pool
-		git checkout tags/v4.9
 		mkdir build && cd build
 
 #Implement merge minign donation choice
