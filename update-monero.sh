1527b3b482bc37e47c2803a88f0778e0f47336c6
shermand100 <shermand100@gmail.com>
2025-08-27 10:44:00 +0100

    Fix formatting on Monero version number

--- update-monero.sh
+++ update-monero.sh
@@ -153,11 +153,11 @@ mkdir .bitmonero 2>&1 | tee -a /home/pinodexmr/debug.log
 #Clean-up used downloaded files
 rm -R ~/temp
 
-		#Update system version number
-		echo "#!/bin/bash
-		CURRENT_VERSION=$NEW_VERSION" > /home/pinodexmr/current-ver.sh
-		#cleanup old version number file
-		rm /home/pinodexmr/xmr-new-ver.sh
+#Update system version number
+echo "#!/bin/bash
+CURRENT_VERSION=$NEW_VERSION" > /home/pinodexmr/current-ver.sh
+#cleanup old version number file
+rm /home/pinodexmr/xmr-new-ver.sh
 
 
 
